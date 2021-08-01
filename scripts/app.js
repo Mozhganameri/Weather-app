3991795c53bfd75ac893a6149a13091b6a0c2e6e
Mozhganameri <86070437+Mozhganameri@users.noreply.github.com>
2021-08-02 02:43:43 +0430

    added js functions for time and date

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,36 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp)
+    let Day = ["Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday"]
+    let Month = ["Jan" , "Feb" , "Mar" , "Apr" , "May" , "Jun" , "Jul" , "Aug" , "Sep" , "Oct" , "Nov" , "Dec"]
+    let day = Day[date.getDay()]
+    let month = Month[date.getMonth()]
+    let monthDay = date.getDate()
+    return `${day}, ${month} ${monthDay}`
+}
+function formatTime(timestamp){
+    let date = new Date(timestamp)
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    if (hours < 10) {
+        hours = `0${hours}`
+    }
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
+    return `${hours} : ${minutes}`
+}
 
 function displayTemperature(response){
     console.log(response)
     let cityElement = document.querySelector("#selectedCity")
-    let temperatureElement = document.querySelector("#currentTime")
+    let temperatureElement = document.querySelector("#currentTemp")
+    let dateElement = document.querySelector("#date")
+    let timeElement = document.querySelector("#time")
     cityElement.innerHTML= response.data.name
     temperatureElement.innerHTML=`${Math.round(response.data.main.temp)}°C`
+    dateElement.innerHTML = formatDate(response.data.dt * 1000)
+    timeElement.innerHTML = formatTime(response.data.dt * 1000)
+
 }
 
 
